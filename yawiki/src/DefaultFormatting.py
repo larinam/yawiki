c5d9ead1c7919d244dd4871d78abda1c14117352
Hatter <krater@narod.ru>
2010-05-03 20:08:57 +0600

    formattings

--- yawiki/src/DefaultFormatting.py
+++ yawiki/src/DefaultFormatting.py
@@ -5,6 +5,6 @@ Created on 03.05.2010
 '''
 from WikiSettingsModels import PageFormattingSetting
 formatting = []
-formatting.append(PageFormattingSetting(pattern=r"'''text'''", regex_pattern=r"'''([\w/<>]+)'''", target=r"<i>\1</i>"))
-formatting.append(PageFormattingSetting(pattern=r"''text''", regex_pattern=r"''([\w/<>]+)''", target=r"<strong>\1</strong>"))
+formatting.append(PageFormattingSetting(pattern=r"'''text'''", regex_pattern=r"'''([\w/<>\s]+)'''", target=r"<i>\1</i>"))
+formatting.append(PageFormattingSetting(pattern=r"''text''", regex_pattern=r"''([\w/<>\s]+)''", target=r"<strong>\1</strong>"))
 formatting.append(PageFormattingSetting(pattern=r"''text''", regex_pattern=r"\[BR\]", target=r"<br />"))
